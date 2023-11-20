c645e77781329ed2fc3ac58c368094d78d0746b9
Lucas Maciel <lucasmacielcontato@gmail.com>
2023-11-20 18:22:47 -0300

    Maioria das funções feitas

--- index.js
+++ index.js
@@ -1,49 +1,28 @@
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const db = require('./queries')
-const Pool = require('pg').Pool
-const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
 
-const pool = new Pool({
-  user: 'me',
-  host: 'localhost',
-  database: 'api',
-  password: 'me1234',
-  port: 5432
-})
+const qr = require('./queries');
 
-const app = express()
-const port = 3000
-
-app.use(bodyParser.json())
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
-    extended: true
+    extended: true,
   })
+);
+
+app.get('/', (req, res) => res.json(
+  { info: 'API is working correctly' })
 )
 
-const createUser = (request, response) => {
-  const { name, points } = request.body
-
-  pool.query('INSERT INTO records (name, points) VALUES ($1, $2) RETURNING *', [name, points], (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(201).send(`User added with ID: ${results.rows[0].id}`)
-  })
-
-  response.json({a: "a"})
-}
-
-app.get('/', (request, response) => {
-  response.json({info: 'Mim dê o cuzinho'})
-})
-
-app.post('/register/:name/:points', createUser)
+app.get('/records', qr.getRecords);
+app.get('/records/:name', qr.getUserRecords);
+app.post('/saveRecord', qr.saveRecord);
+app.post('/createUser', qr.createUser)
+app.get('/users/login/:nick/:password', qr.login)
 
 app.listen(port, () => {
-  console.log("App rodando")
-})
-
+  console.log(`App running on port ${port}.`);
+});
