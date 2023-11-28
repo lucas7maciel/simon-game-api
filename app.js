5dda8ed9020551acf65817429fcb6eef7aa048bf
Lucas Maciel <lucasmacielcontato@gmail.com>
2023-11-28 09:48:47 -0300

    Dotenv

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const load = require('express-load')
 const Pool = require('pg').Pool
+require('dotenv').config()
 
 const app = express();
 const port = process.env.port
