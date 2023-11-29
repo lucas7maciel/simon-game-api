9faa13387a93c6f902d02ce9d04d95e5ce4d4c13
Lucas Maciel <lucasmacielcontato@gmail.com>
2023-11-29 15:48:36 -0300

    Att 8

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
-import app from "../index"
+const app = require("../index.js")
+const load = require('express-load')
 
 load('public/controllers')
 .then('public/routes')
 .into(app)
 
-export default app
+module.exports = app
