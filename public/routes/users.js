1baf423e621825bca7e8bb4d098e72a9b3e78eca
Lucas Maciel <lucasmacielcontato@gmail.com>
2023-11-30 12:15:01 -0300

    Att 18

--- public/routes/users.js
+++ public/routes/users.js
@@ -1,6 +1,6 @@
-module.exports = (app) => {
+/*module.exports = (app) => {
   const funcs = app.public.controllers.users
 
   app.post('/users/signup', funcs.signUp)
   app.get('/users/signin/:nick/:password', funcs.signIn) //tirar esses argumentos na url
-}
+}*/
